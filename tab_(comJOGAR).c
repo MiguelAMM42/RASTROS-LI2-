0cc6a8efdb5b62eead445f9cde14e1a1c1b3ad01
gonaloppcc <62038187+gonaloppcc@users.noreply.github.com>
2020-03-11 15:47:02 +0000

    Update tab_(comJOGAR).c

--- tab_(comJOGAR).c
+++ tab_(comJOGAR).c
@@ -1,33 +1,5 @@
 #include "dados.h"
 
-void desenhaTabuleiro (ESTADO *s) {
-    int i = 0;
-    int linha = 0;
-    for (linha = 0; linha < 8; linha++)
-    {
-     for (i = 0; i < 9; i++){
-        if (i == 8) putchar('\n');
-        else{
-         if (i == 7 && linha == 0) putchar ('2');
-         else {
-            if (i == 0 && linha == 7) putchar ('1');
-                else {
-                        if ((s -> tab [linha] [i]) == VAZIO) putchar ('#');    // &(s -> registos[(s -> jogadas)
-                        else {
-                            if ((s -> tab [linha] [i]) == BRANCA) putchar ('.');
-                            else putchar ('*'); 
-                            }
-                        }
-
-                }
-        }
-    }
-}
-}
-   
-
-
-
 int jogadaValida(ESTADO *teste1, COORDENADA c){
     //primeiro ve se ajogada é valida
     //depois modifica o estado se for válida
