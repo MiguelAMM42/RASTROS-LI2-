c02048f6aef668cef3e22885c5cd7cca3102dee5
MiguelAMM42 <56049132+MiguelAMM42@users.noreply.github.com>
2020-03-12 16:46:40 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -35,24 +35,37 @@ int jogar(ESTADO *teste1, COORDENADA c) {
     //primeiro ve se a jogada é valida
     //depois modifica o estado se for válida
     // 1 para válida; 0 n valida    
-    int b, n, m, y , z, j;
+    int b, n, m, y , z, j, p;
     j = jogadaValida(teste1, c);
-    if (!j) return 0;
-    b = teste1 -> jogador_atual  ;
-    y = (teste1 -> ultima_jogada).coluna;
-    z = (teste1 -> ultima_jogada).linha;
-    n = c.coluna;
-    m = c.linha;
-    teste1 -> tab [z] [y] = PRETA; // ALTERA A CASA PARA PRETA, VERIFICAR SE É Y / Z OU Z / Y
-    teste1 -> tab [m] [n] = BRANCA; // IGUAL A CIMA
+    p = casaVazia(teste1, c);
+    if (!p)return 0;
+    if (!j)return 0;
+    b = teste1 -> jogador_atual ;   //JOGADOR ATUAL
+    y = (teste1 -> ultima_jogada).coluna;   // COLUNA ULTIMA JOGADA
+    z = (teste1 -> ultima_jogada).linha;   // LINHA ULTIMA JOGADA
+    n = c.coluna;   // COLUNA DA JOGADA
+    m = c.linha;   // LINHA DA JOGADA
+    teste1 -> tab [z] [y] = BRANCA; // ALTERA A CASA PARA PRETA, VERIFICAR SE É Y / Z OU Z / Y
+    teste1 -> tab [m] [n] = PRETA; // IGUAL A CIMA
     // Alterar no array de jogadas, acrescentar a atual
     int jogadaAtual;
     jogadaAtual = (teste1->num_jogadas);
+    if (b == 1) {
+    ((teste1 -> jogadas[jogadaAtual].jogador1).coluna) = n;
+    ((teste1 -> jogadas[jogadaAtual].jogador1).linha) = m;
+                }
+    else        {
     ((teste1 -> jogadas[jogadaAtual].jogador2).coluna) = n;
-    ((teste1 -> jogadas[jogadaAtual].jogador2).linha) = m;
+    ((teste1 -> jogadas[jogadaAtual].jogador2).linha) = m; 
+                }
 
-    (teste1 -> num_jogadas) ++;
+    (teste1->ultima_jogada).coluna = n;
+        (teste1->ultima_jogada).linha = m;  
+    (teste1 -> num_jogadas) = jogadaAtual + 1;
     if ( b == 2) teste1 -> jogador_atual = 1;
     else teste1 -> jogador_atual = 2;
+    printf ("\n jogador atual %d", (teste1 -> jogador_atual));
+    printf ("\n num jogadas %d \n", jogadaAtual);
+    
     return 1;
 }
