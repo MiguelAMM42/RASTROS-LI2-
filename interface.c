14187963e15ab89e94e2d4d41969e521f575d58c
MiguelAMM42 <56049132+MiguelAMM42@users.noreply.github.com>
2020-03-12 16:34:50 +0000

    Update interface.c

--- interface.c
+++ interface.c
@@ -6,61 +6,69 @@
 
 
 ESTADO *inicializar_estado() {
-    ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
-    e -> jogador_atual = 1;
-    e -> num_jogadas = 0;
-    for (int i = 0; i < 8; i++) {
-        for (int ii = 0; ii < 8; ii ++) {
-            if (i == 3 && ii == 4) (e -> tab [i][ii]) = PRETA;
-            else (e -> tab [i][ii]) = VAZIO;                                                    
-        }
-    }
-    return e;
+    ESTADO *teste1 = (ESTADO *) malloc(sizeof(ESTADO));
+    teste1->jogador_atual = 1;
+    teste1->num_jogadas = 0;
+    teste1->ultima_jogada.coluna = 4;
+    teste1->ultima_jogada.linha = 3;
+
+    
+
+    // Falta defenir as primeiras jogadas, são precisas informações dos dois jogadores
+    for (int i = 0; i < 8; i++) {for (int ii = 0; ii < 8; ii ++) 
+                                                {   if (i == 3 && ii == 4) (teste1-> tab [i] [ii]) = PRETA;
+                                                    else (teste1->tab [i] [ii]) = VAZIO;
+                                                        
+                                                }
+                                         }
+    return teste1;
 }
 
 
 void desenhaTabuleiro (ESTADO *s) {
     int i = 0;
     int linha = 0;
-    for (linha = 0; linha < 8; linha++) {
-        for (i = 0; i < 9; i++) {
-            if (i == 8) putchar('\n');
-            else {
-                if (i == 7 && linha == 0) putchar ('2');
+    for (linha = 0; linha < 8; linha++)
+    {
+     for (i = 0; i < 9; i++)
+     {
+        if (i == 8) putchar('\n');
+        else{
+         if (i == 7 && linha == 0) putchar ('2');
+         else {
+            if (i == 0 && linha == 7) putchar ('1');
                 else {
-                    if (i == 0 && linha == 7) putchar ('1');
-                    else {
-                        if ((s -> tab [linha] [i]) == PRETA) putchar ('#');    // &(s -> registos[(s -> jogadas)
+                        if ((s -> tab [linha] [i]) == BRANCA) putchar ('#');    // &(s -> registos[(s -> jogadas)
                         else {
                             if ((s -> tab [linha] [i]) == VAZIO) putchar ('.');
                             else putchar ('*'); 
-                        }
-                    }
-                }
+                            }
+                     }
+
+              }
             }
-        }
     }
+}
 }
 
 void mostra_jogadas (ESTADO *s)  // Imprime a lista de jogadas
 { // Falta inicializar a lista de jogadas
     int jogadaNum = 0;
-    while (jogadaNum < (s -> num_jogadas) -1) {
+    while (jogadaNum < (s -> num_jogadas) - 1 ) {
         printf ("%d : ", jogadaNum);
-        printf ("%c %d / ", (((s -> jogadas[jogadaNum]).jogador1).coluna) + 96, (((s -> jogadas[jogadaNum]).jogador1).linha)); 
-        printf ("%c %d ;\n", (((s -> jogadas[jogadaNum]).jogador2).coluna) + 96, (((s -> jogadas[jogadaNum]).jogador2).linha));
-        jogadaNum++;
+        printf ("%d %d / ", (((s -> jogadas[jogadaNum]).jogador1).coluna) /*+ 96*/, (((s -> jogadas[jogadaNum]).jogador1).linha)); 
+        printf ("%d %d ;\n", (((s -> jogadas[jogadaNum+1]).jogador2).coluna) /*+ 96*/, (((s -> jogadas[jogadaNum+1]).jogador2).linha));
         jogadaNum++;
     }
-    if (jogadaNum == (s -> num_jogadas) -1)  
-        {
+ //   if (jogadaNum == (s -> num_jogadas) -1)
+   //     {
             if ((s -> jogador_atual) == 1) printf ("é a vez do jogador 1!");
             else {
                     printf ("%d : ", jogadaNum);
-                    printf ("%c %d / ", (((s -> jogadas[jogadaNum-1]).jogador1) .coluna) + 96, (((s -> jogadas[jogadaNum-1]).jogador1) .linha)); 
+                    printf ("%d %d / ", (((s -> jogadas[jogadaNum-1]).jogador1) .coluna) + 96, (((s -> jogadas[jogadaNum-1]).jogador1) .linha)); 
                     printf ("È a vez do jogador 2!\n");
                  }
         
-        }
+    //    }
         
 }
