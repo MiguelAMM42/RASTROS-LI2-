56e75ac0e6c7b766f9a163761296729c5a496b73
MiguelAMM42 <56049132+MiguelAMM42@users.noreply.github.com>
2020-03-12 16:12:16 +0000

    Update logica.h

--- logica.h
+++ logica.h
@@ -1,6 +1,7 @@
 #ifndef ___LOGICA_H___
 #define ___LOGICA_H___
 
+int casaVazia(ESTADO *teste1, COORDENADA c);
 int jogar (ESTADO *teste1, COORDENADA c);
 int jogadaValida (ESTADO *teste1, COORDENADA c);
 
