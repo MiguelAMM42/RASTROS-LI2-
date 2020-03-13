8f2b75005130d9ded953a2278ce4782a0a21965b
Diogo Manuel Brito Pires <30441515+morteaobot@users.noreply.github.com>
2020-03-13 14:45:18 +0000

    Update interface.c

--- interface.c
+++ interface.c
@@ -61,3 +61,15 @@ void mostra_jogadas (ESTADO *s) {  // Imprime a lista de jogadas
         printf ("È a vez do jogador 2!\n");
     }        
 }
+
+int interpretador(ESTADO *e) {
+    char linha[BUF_SIZE];
+    char col[2], lin[2];
+    if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0;
+    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
+        COORDENADA coord = {*col - 'a', *lin - '1'};
+        jogar(e, coord);
+        mostrar_tabuleiro(e);
+    }
+    return 1;
+}
