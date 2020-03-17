8781c4fb0e79f24b6e81dcc3fab6004ddd0addc8
Miguel Ângelo Machado Martins <56049132+MiguelAMM42@users.noreply.github.com>
2020-03-17 21:38:00 +0000

    Update interface.c

--- interface.c
+++ interface.c
@@ -6,21 +6,21 @@
 #include "logica.h"
 #include "interface.h"
 
-
 ESTADO *inicializar_estado() {
     ESTADO *teste1 = (ESTADO *) malloc(sizeof(ESTADO));
     teste1 -> jogador_atual = 1;
     teste1 -> num_jogadas = 0;
     teste1 -> ultima_jogada.coluna = 4;
-    teste1 -> ultima_jogada.linha = 3;
-    for (int i = 0; i < 8; i++) {
-        for (int ii = 0; ii < 8; ii ++) {
-            if (i == 3 && ii == 4) (teste1 -> tab [i] [ii]) = BRANCA;
-            else (teste1 -> tab [i] [ii]) = VAZIO;                                          
+    teste1 -> ultima_jogada.linha = 5;
+    for (int i = 8; i > 0; i--) {                                         ///
+        for (int ii = 0; ii < 8; ii ++) {                                 ///
+            if (i == 5 && ii == 4) (teste1 -> tab [i] [ii]) = BRANCA;     ///
+            else (teste1 -> tab [i] [ii]) = VAZIO;                        ///                  
         }
     }
     return teste1;
 }
+
 
 
 void mostrar_tabuleiro (ESTADO s) {
