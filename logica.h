a0956d80d5f0faff93c17dab2f9fb6a1747428c2
Diogo Manuel Brito Pires <30441515+morteaobot@users.noreply.github.com>
2020-03-18 19:34:11 +0000

    Update logica.h

--- logica.h
+++ logica.h
@@ -1,6 +1,8 @@
 #ifndef ___LOGICA_H___
 #define ___LOGICA_H___
 
+
+
 int casaVazia (ESTADO *estado, COORDENADA c);
 int jogar (ESTADO *estado, COORDENADA c);
 int jogadaValida (ESTADO *estado, COORDENADA c);
