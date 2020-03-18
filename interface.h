62245e3452361026a7fe6e67006e25e04833bcdb
Diogo Manuel Brito Pires <30441515+morteaobot@users.noreply.github.com>
2020-03-18 19:34:43 +0000

    Update interface.h

--- interface.h
+++ interface.h
@@ -2,9 +2,11 @@
 #define ___INTERFACE_H___
 
 #define BUF_SIZE 1024
-void mostra_jogadas (ESTADO *s);
-void mostrar_tabuleiro (ESTADO s);
+void mostra_jogadas (ESTADO *s, FILE *fp);
+void mostrar_tabuleiro (ESTADO s, FILE *fp);
 ESTADO *inicializar_estado();
 int interpretador(ESTADO *e);
+
+void guarda_ficheiro (ESTADO *e, FILE *fp);
 
 #endif
