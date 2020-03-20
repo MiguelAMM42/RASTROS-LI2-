35eda78a7d3eea9858a367caf7ded68928c33b0b
Diogo Manuel Brito Pires <30441515+morteaobot@users.noreply.github.com>
2020-03-20 15:46:45 +0000

    Documentação

--- dados.h
+++ dados.h
@@ -67,25 +67,70 @@ Esta função inicializa o valor do estado. Isso implica o tabuleiro ser colocad
 @returns O novo estado
 */
 ESTADO *inicializar_estado();
-
+/**
+\brief Altera a CASA numa dada coordenada
+@param e Apontador para o Estado
+@param s Apontador para o Coordenada
+@param V Apontador para o tipo de CASA Novo
+*/
 void set_casa (ESTADO *e, COORDENADA c, CASA V);
-
+/**
+\brief Lê o tipo de casa de uma dada coordenada
+@param e Apontador para o Estado
+@param c Coordenada para procurar no Estado
+@returns CASA da coordenada
+*/
 CASA get_casa (ESTADO *e, COORDENADA c);
-
+/**
+\brief Lê a última jogada a partir do Estado
+@param e Apontador para o Estado
+@param c Coordenada para procurar no Estado
+*/
 void set_ultima_jogada (ESTADO *e, COORDENADA c);
-
+/**
+\brief Lê a Coordenada da última jogada
+@param e Apontador para o Estado
+@returns COORDENADA da última jogada
+*/
 COORDENADA get_ultima_jogada (ESTADO *e);
-
+/**
+\brief Altera o número de jogadas
+@param e Apontador para o Estado
+@param n Número para alterar
+*/
 void set_num_jogadas (ESTADO *e, int n);
-
+/**
+\brief Descobre o número de jogadas
+@param e Apontador para o Estado
+@returns Número de jogadas
+*/
 int get_num_jogadas (ESTADO *e);
-
+/**
+\brief Altera o jogador atual
+@param e Apontador para o Estado
+@param n Número do novo jogador atual
+*/
 void set_jogador_atual (ESTADO *e, int n);
-
+/**
+\brief Altera o jogador atual
+@param e Apontador para o Estado
+@returns Número do jogador atual
+*/
 int get_jogador_atual (ESTADO *e);
-
+/**
+\brief Altera a lista de jogadas, numa posição e de um jogador 
+@param e Apontador para o Estado
+@param num_jogada Número da posição que será alterada, no array JOGADAS
+@param jogador Jogador que será alterado
+@param c Coordenada para acrescentar na lista
+*/
 void set_jogadas_coordenada (ESTADO *e, int num_jogada, int jogador, COORDENADA c);
-
+/**
+\brief Compara duas coordenadas 
+@param a Coordenada
+@param b Coordenada
+@returns TRUE (1) ou FALSE (0), se as coordenadas são iguais ou não
+*/
 int coordenadaIgual (COORDENADA a, COORDENADA b);
 
 #endif
