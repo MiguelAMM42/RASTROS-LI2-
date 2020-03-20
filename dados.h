e6749b389fb4ddadb361199c4dce8acdb854bfc8
Diogo Manuel Brito Pires <30441515+morteaobot@users.noreply.github.com>
2020-03-20 10:12:14 +0000

    Documentação dos dados

--- dados.h
+++ dados.h
@@ -1,31 +1,70 @@
+/**
+@file dados.h
+Definição do estado e das funções que o manipulam
+*/
+
 #ifndef ___DADOS_H___
-#define ___DADOS_H___
+#define  ___DADOS_H___
+#define BUF_SIZE 1024
 
+/**
+\brief Tipo de dados para a casa
+*/
+typedef enum {
+  UM = '1',
+  DOIS = '2',
+  VAZIO = '.',
+  BRANCA = '*',
+  PRETA = '#'
+} CASA;
 
-#define TRUE 1
-#define FALSE 0
-#define MAX 64
-
-typedef enum {VAZIO, BRANCA, PRETA, UM, DOIS} CASA;
-
+/**
+\brief Tipo de dados para as coordenadas
+*/
 typedef struct {
     int coluna;
-    int linha; 
+    int linha;
 } COORDENADA;
 
-
+/**
+\brief Tipo de dados para a jogada
+*/
 typedef struct {
-        COORDENADA jogador1;
-        COORDENADA jogador2;
+    COORDENADA jogador1;
+    COORDENADA jogador2;
 } JOGADA;
 
+/**
+\brief Tipo de dados para as jogadas
+*/
 typedef JOGADA JOGADAS[32];
+
+/**
+\brief Tipo de dados para o estado
+*/
+
 typedef struct {
-        CASA tab[8][8];
-        COORDENADA ultima_jogada;
-        JOGADAS jogadas;
-        int num_jogadas;
-        int jogador_atual;
+  /** O tabuleiro */
+    CASA tab[8][8];
+  /** As jogadas */
+    JOGADAS jogadas;
+  /** O número das jogadas, usado no prompt */
+    int num_jogadas;
+  /** O jogador atual */
+    int jogador_atual;
+  /** O nº de comando, usado no prompt */
+  int num_comando;
+  /** A coordenada da última jogada */
+  COORDENADA ultima_jogada;
 } ESTADO;
 
+/**
+\brief Inicializa o valor do estado
+Esta função inicializa o valor do estado. Isso implica o tabuleiro ser colocado na posição inicial e todos os campos do estado estarem com o valor por omissão.
+@returns O novo estado
+*/
+ESTADO *inicializar_estado();
+
 #endif
+
+
