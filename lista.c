0e2db3c70cd10e7ad9d3a992bc39a91cdf876ad5
gonaloppcc <goncalosoares555@gmail.com>
2020-04-03 20:49:54 +0100

    Lista.h funcional e função lista_esta_vazia adicionada

--- lista.c
+++ lista.c
@@ -6,36 +6,43 @@
 #define BUF_SIZE 1024
 
 int main() {
-  LISTA L = criar_lista();
-  char linha[BUF_SIZE];
-    
-  printf("Insira várias linhas, acabando com CTRL-D:\n");
+	LISTA L = criar_lista();
+	char linha[BUF_SIZE];
+	printf("Insira várias linhas, acabando com CTRL-D:\n");	
+	// control-D é a tecla CTRL e a tecla D ao mesmo tempo
+	// Em windows é capaz de ser CTRL-Z
+	while(fgets(linha, BUF_SIZE, stdin) != 0) {
+		// A função strdup cria uma cópia da string que foi lida
+	    L = insere_cabeca(L, strdup(linha));
+	}
 
-  // control-D é a tecla CTRL e a tecla D ao mesmo tempo
-  // Em windows é capaz de ser CTRL-Z
-    while(fgets(linha, BUF_SIZE, stdin) != 0) {
-        // A função strdup cria uma cópia da string que foi lida
-        L = insere_cabeca(L, strdup(linha));
-    }
-
-  printf("\n==============================\n");
-  printf(  "=          PERCURSO          =\n");
-  printf(  "==============================\n\n");
-    // percorre sem remover os elementos da lista
-    for(LISTA T = L; !lista_esta_vazia(T); T = proximo(T)) {
-        char *str = (char *) devolve_cabeca(T);
-        printf("%s", str);
-    }
-    
-  printf("\n==============================\n");
-  printf(  "=           REMOCAO          =\n");
-  printf(  "==============================\n\n");
-    // percorre e vai removendo a cabeça
-    while(!lista_esta_vazia(L)) {
-        char *str = (char *) devolve_cabeca(L);
-        L = remove_cabeca(L);
-        printf("%s", str);
-        free(str);
-    }
-  return 0;
+	printf("\n==============================\n");
+	printf(  "=          PERCURSO          =\n");
+	printf(  "==============================\n\n");
+	
+	// percorre sem remover os elementos da lista
+	for (LISTA T = L; !lista_esta_vazia(T); T = proximo(T)) {
+		char *str = (char *) devolve_cabeca(T);
+	  	printf("%s", str);
+	}
+	
+	printf("\n==============================\n");
+	printf(  "=           REMOCAO          =\n");
+	printf(  "==============================\n\n");
+	// percorre e vai removendo a cabeça
+	while(!lista_esta_vazia(L)) {
+	    char *str = (char *) devolve_cabeca(L);
+	    L = remove_cabeca(L);
+	    printf("%s", str);
+	    free(str);
+	}
+	return 0;
 }
+
+int lista_esta_vazia (LISTA L) {
+	if (L == NULL) return 1;
+	return 0;
+	// OU (?)
+	// return L;
+}
+
--- lista.h
+++ lista.h
@@ -8,25 +8,25 @@ Definição das funções relacionadas com as listas.
 
 #define BUF_SIZE 1024
 
-typedef struct *LISTA;
-
 typedef struct
 {
 	COORDENADA c;
 	int valor;
 } COOR;
 
-typedef struct 
+typedef struct slist
 {
 	COOR c;
-	LISTA prox;
-}Nodo;
+	struct slist *prox;
+} Nodo;
 
-LISTA criar_lista();
-LISTA insere_cabeca(LISTA L, void *valor);
-void *devolve_cabeca(LISTA L);
-LISTA proximo(LISTA L);
-LISTA remove_cabeca(LISTA L);
+typedef Nodo *LISTA;
+
+// LISTA criar_lista();
+// LISTA insere_cabeca(LISTA L, void *valor);
+// void *devolve_cabeca(LISTA L);
+// LISTA proximo(LISTA L);
+// LISTA remove_cabeca(LISTA L);
 int lista_esta_vazia(LISTA L);
 
 #endif
