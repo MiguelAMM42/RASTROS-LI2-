90f48f74a37f7dbee5cfc94e3bc7452041697dfa
Miguel Ângelo Machado Martins <56049132+MiguelAMM42@users.noreply.github.com>
2020-04-07 13:32:34 +0100

    Funções proximo e insere_cabeca

--- lista.h
+++ lista.h
@@ -23,10 +23,15 @@ typedef struct slist
 typedef Nodo *LISTA;
 
 // LISTA criar_lista();
-// LISTA insere_cabeca(LISTA L, void *valor);
+LISTA insere_cabeca(LISTA L, void *valor);
 // void *devolve_cabeca(LISTA L);
-// LISTA proximo(LISTA L);
+LISTA proximo(LISTA L);
 // LISTA remove_cabeca(LISTA L);
 int lista_esta_vazia(LISTA L);
 
+
+
 #endif
+
+
+
