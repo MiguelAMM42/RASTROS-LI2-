1f1d998056ffb0e85270a4726d57612ac788551c
Miguel Ângelo Machado Martins <56049132+MiguelAMM42@users.noreply.github.com>
2020-04-19 19:01:10 +0100

    Comentários adicionados às funções das listas

--- lista.h
+++ lista.h
@@ -30,19 +30,15 @@ typedef struct nodo {
    void *valor;
    struct nodo *prox;
 } NODO, *LISTA;
-<<<<<<< HEAD
 
 
-//COMENTAR FUNÇÕES TODAS-------------///
 
-=======
 /**
 \brief Inicializa a lista, com NULL
 @param e Apontador para o Estado
 @param c Apontador para o Coordenada
 @param l Apontador para uma lista
 */
->>>>>>> 1ca75f6ad4ec0af002a711c3a86e2034238931f6
 LISTA criar_lista();
 /**
 \brief Acrescenta um valor ao início da lista
@@ -50,28 +46,46 @@ LISTA criar_lista();
 @param valor Endereço do valor para acrescentar 
 */
 LISTA insere_cabeca(LISTA L, void *valor);
-
-void *devolve_cabeca(LISTA L);
-
-LISTA proximo(LISTA L);
-
-LISTA remove_cabeca(LISTA L);
-
-int lista_esta_vazia(LISTA L);
-
-LISTA criaLista (ESTADO *e);
-<<<<<<< HEAD
-LISTA adicionarCoordenada (ESTADO *e, COORDENADA c, LISTA l);
 /**
-\brief Calcula rapidamente a distância da coordenada à peça do jogador Atual
+brief Devolve a cabeça da lista
+@param L Lista a ver
+@param valor Apontador para a cabeça da Lista
+*/
+void *devolve_cabeca(LISTA L);
+/**
+brief Devolve a cauda da lista
+@param L Lista à qual se vai remover a cabeça
+*/
+LISTA proximo(LISTA L);
+/**
+brief Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+@param L Lista na qual se vai retirar a cabeça e libertar espaço
+*/
+LISTA remove_cabeca(LISTA L);
+/**
+brief Diz se a Lista está vazia(devolvendo 1) ou não (devolvendo 0)
+@param L Lista a verificar se está ou não vazia
+*/
+int lista_esta_vazia(LISTA L);
+/**
+\brief Cria uma lista com as coordenadas de possiveis jogadas, para posteriormente criar uma heurística
+@param e Apontador para o Estado
+*/
+LISTA criaLista (ESTADO *e);
+/**
+\brief Calcula rapidamente a distância da coordenada à peça de vitória do jogador Atual
 @param c Coordenada onde se encontra a peça branca
 @param JOgador Atual (1 ou 2)
 */
 int dist_jog1 (COORDENADA c, int jogAtual);
-=======
-
+/**
+\brief Caso a coordenada seja uma jogada válida, adiciona à lista
+@param Apontador para o Estado
+@param Apontador para a Coordenada
+@param Apontador para a Lista
+*/
 void adicionarCoordenada (ESTADO *e, COORDENADA *c, LISTA *l);
->>>>>>> 1ca75f6ad4ec0af002a711c3a86e2034238931f6
+
 
 
 
