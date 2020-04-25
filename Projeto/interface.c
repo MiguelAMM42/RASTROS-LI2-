a55492464ad46f21093e63461f895cdd5bac90a9
Miguel Ângelo Machado Martins <56049132+MiguelAMM42@users.noreply.github.com>
2020-04-25 21:19:50 +0100

    Update interface.c

--- Projeto/interface.c
+++ Projeto/interface.c
@@ -104,7 +104,7 @@ int comandoGravar(ESTADO *e, const char *endereco ){
             }
             guarda_ficheiro (e, fp);
             fclose(fp);
-            printf("Ficheiro %s guardado!\n", endereco);
+            printf("\nFicheiro %s guardado!\n\n", endereco);
             e -> num_comando ++;
             return 1;
         }
