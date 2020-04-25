2a96763453b7648f60f128cdcc72030678deff55
Miguel Ângelo Machado Martins <56049132+MiguelAMM42@users.noreply.github.com>
2020-04-25 19:33:03 +0100

    Update interface.h

--- Projeto/interface.h
+++ Projeto/interface.h
@@ -36,5 +36,40 @@ void mostra_prompt (ESTADO *s);
 @param fp Apontador para o ficheiro onde se guarda o Estado
 */
 void guarda_ficheiro (ESTADO *e, FILE *fp);
+/**
+\brief Usado no Interpretador para executar o comando "pos"
+@param e Apontador para o Estado
+@param jog Número do conjunto de jogadas para as quais se quer retornar
+@returns Valor que corresponde ao tipo de comportamento que deve ter o Jogo.
+*/
+int comandoPos(ESTADO *e, int jog);
+/**
+\brief Usado no Interpretador para executar uma jogada(ex.:"e4")
+@param e Apontador para o Estado
+@param col Apontador para a coluna onde se vai jogar
+@param lin Apontador para a linha onde se vai jogar
+@returns Valor que corresponde ao tipo de comportamento que deve ter o Jogo.
+*/
+int comandoJogar(ESTADO *e, char *col, char *lin);
+/**
+\brief Usado no Interpretador para executar o comando "gr"
+@param e Apontador para o Estado
+@param endereco Apontador para o endero do ficheiro onde se guarda o Estado
+@returns Valor que corresponde ao tipo de comportamento que deve ter o Jogo.
+*/
+int comandoGravar(ESTADO *e, const char *endereco );
+/**
+\brief Usado no Interpretador para executar o comando "jog"
+@param e Apontador para o Estado
+@returns Valor que corresponde ao tipo de comportamento que deve ter o Jogo.
+*/
+int comandoJog (ESTADO *e);
+/**
+\brief Usado no Interpretador para executar o comando "ler"
+@param e Apontador para o Estado
+@param endereco Apontador para o endereço do ficheiro a ler
+@returns Valor que corresponde ao tipo de comportamento que deve ter o Jogo.
+*/
+int comandoLer(ESTADO *e, const char *endereco );
 
 #endif
